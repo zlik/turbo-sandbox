f6d0d2c37172a3a3194bfeabdb0d14a3629d4ab9
zlik <zlikwon@gmail.com>
2013-10-01 18:54:41 +0300

    added loop experiment

---
+++ loop.php
@@ -0,0 +1,54 @@
+<?php
+ini_set('memory_limit', '512M');
+
+$getProductIds = function ($number) {
+    $productIds = array();
+    $collection = Mage::getResourceModel('catalog/product_collection');
+    $collection->getSelect()->limit($number);
+    foreach ($collection as $product) {
+        $productIds[] = $product->getId();
+    }
+    $collection->clear();
+    return $productIds;
+};
+
+
+$exp1 = function ($productIds) {
+    $start = microtime(true);
+    $r = array();
+    $collection = Mage::getResourceModel('catalog/product_collection')
+        ->addFieldToFilter('entity_id', array($productIds))
+        ->addAttributeToSelect(array('name'));
+    foreach ($collection as $product) {
+        $r[] = $product->getName();
+    }
+    $end = microtime(true);
+    $collection->clear();
+    return $end - $start;
+};
+
+$exp2 = function ($productIds) {
+    $start = microtime(true);
+    $r = array();
+    foreach ($productIds as $productId) {
+        $product = Mage::getModel('catalog/product')->load($productId);
+        $r[] = $product->getName();
+    }
+    $end = microtime(true);
+    return $end - $start;
+};
+
+echo '<table><tr><td># of Products</td><td>Collection</td><td>Model Loop</td></tr>';
+for ($i = 10; $i <= 1000; $i = $i + 20) {
+    $start = microtime(true);
+    $productIds = $getProductIds($i);
+    $end = microtime(true);
+
+    echo '<tr>';
+    echo '<td>' . $i . '</td>';
+    echo '<td>' . ($exp1($productIds) + ($end - $start)) . '</td>';
+    echo '<td>' . $exp2($productIds) . '</td>';
+    echo '</tr>';
+}
+echo '</table>';
+die();
